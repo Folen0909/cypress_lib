7ca9088988f52a17865e8b2cb5bc74ca3fad62af
David Turkalj <davidturkalj@gmail.com>
2022-08-17 14:45:20 +0200

    Setup completed

---
+++ cypress.config.ts
@@ -0,0 +1,18 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    redirectionLimit: 50,
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    numTestsKeptInMemory: 100,
+    experimentalSessionAndOrigin: true,
+    baseUrl: "https://www.demoblaze.com/",
+    env: {
+      "email" : "",
+      "password": "",
+      "locale": "en-US",
+      "specialDateFormat": ""
+    }
+  },
+});
